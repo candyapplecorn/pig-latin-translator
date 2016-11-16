3da4bcb06f3bab6fc93dd046a150c73b0808c7cf
candyapplecorn <candyapplecorn@gmail.com>
2016-11-15 16:31:38 -0800

    more shebang nonsense

--- pipeto.js
+++ pipeto.js
@@ -1,4 +1,8 @@
-#!/usr/bin/node
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+
+// https://unix.stackexchange.com/questions/65235/universal-node-js-shebang
+
 var path = require('path'), pl;
 try       { pl = require('pigLatin');               }
 catch (e) { pl = require(path.resolve('pigLatin')); }
